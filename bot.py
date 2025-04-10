882506e4a078a85e211c3b49e808369666ec0f8a
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-10 10:52:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,7 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-async def start(update, context):
-    await update.message.reply_text("Bot çalışıyor!")
+async def handle_message(update, context):
+    await update.message.reply_text("Nötr")  # test için böyleydi, analiz kodun burada olacak
 
-def start_bot():
-    import asyncio
-    from telegram.ext import ApplicationBuilder
-
-    application = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
-    application.add_handler(CommandHandler("start", start))
-    asyncio.run(application.run_polling())
+application = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
