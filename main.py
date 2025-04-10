ef0a04723194b63bf36958ab74d214ae09c548fa
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-10 12:21:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
-import os
+from telegram import Update
+from telegram.ext import ContextTypes
 
-async def handle_message(update, context):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    print(f"Gelen mesaj: {text}")
     await update.message.reply_text("Nötr")
 
-application = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
-application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+app = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    application.run_webhook(
+    app.run_webhook(
         listen="0.0.0.0",
-        port=port,
+        port=10000,
         webhook_url="https://kriptocobani.onrender.com"
     )
