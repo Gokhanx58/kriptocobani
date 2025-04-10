a28d136e87757eabba5eba5924c25bb9f3fc1da8
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-10 13:08:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-import os
-from telegram.ext import ApplicationBuilder, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from telegram import Update
-from telegram.ext import ContextTypes
+import os
+
+TOKEN = "7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw"
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Bot çalışıyor. Mesaj alındı.")
+    await update.message.reply_text("Bot çalışıyor! ✅")
 
-application = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
-
+application = ApplicationBuilder().token(TOKEN).build()
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-if __name__ == '__main__':
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
-        url_path="",  # boş bırakılıyor
-        webhook_url="https://kriptocobani.onrender.com"  # sonunda "/" YOK
-    )
+application.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.environ.get("PORT", 10000)),
+    webhook_url="https://kriptocobani.onrender.com"
+)
