31455c4e5ca4f307d041e42185dc4cd286a42e58
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-10 15:13:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from telegram.ext import ApplicationBuilder, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram import Update
+import asyncio
 
-async def handle_message(update, context):
-    await update.message.reply_text("Bot şu anda polling ile çalışıyor.")
+TOKEN = "7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw"
 
-application = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
-application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    await update.message.reply_text(f"Gelen mesaj: {text}")
 
-application.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    print("Bot polling ile çalışıyor...")
+    app.run_polling()
