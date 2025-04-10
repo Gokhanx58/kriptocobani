d94eceb8574bacb8e6c463b8392f55f21e66daac
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-10 11:24:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
+import logging
+
+# Loglama eklendi
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 async def handle_message(update, context):
-    await update.message.reply_text("Nötr")  # test için böyleydi, analiz kodun burada olacak
+    print("Mesaj alındı:", update.message.text)
+    await update.message.reply_text("Nötr")
 
 application = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
