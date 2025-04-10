5e0dc3482823b6e903b242b432f124f64fb226f9
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-10 11:25:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from bot import application
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
 
-if __name__ == '__main__':
-    application.run_webhook(
-        listen="0.0.0.0",
-        port=10000,
-        webhook_url="https://kriptocobani.onrender.com"
-    )
+async def handle_message(update, context):
+    await update.message.reply_text("Nötr")
+
+application = ApplicationBuilder().token("7649989587:AAHUpzkXy3f6ZxoWmNTFUZxXF-XHuJ4DsUw").build()
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
