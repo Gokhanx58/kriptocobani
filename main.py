f501f43f3d31525f2401277b8a0b7f3d66759285
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-12 10:18:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # main.py
 
 import asyncio
+import nest_asyncio
+from telegram.ext import ApplicationBuilder, CommandHandler
+from handlers import analiz_komutu
 from signal_loop import start_signal_loop
 
+BOT_TOKEN = "8002562873:AAHoMdOpiZEi2XILMmrwAOjtyKEWNMVLKcs"
+
+nest_asyncio.apply()
+
 async def main():
-    print("Sinyal kontrol sistemi başlatılıyor...")
-    while True:
-        await start_signal_loop()
-        await asyncio.sleep(180)  # 3 dakika bekle
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("analiz", analiz_komutu))
+    asyncio.create_task(start_signal_loop())
+    print("Bot çalışıyor...")
+    await app.run_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
