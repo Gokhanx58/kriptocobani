01ea6393626c0b2b4696974a603fe748102a96c7
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-12 10:37:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,8 @@ from signal_loop import start_signal_loop
 nest_asyncio.apply()
 
 async def main():
-    # Sadece sinyal kontrol döngüsü başlatılır
-    asyncio.create_task(start_signal_loop())
-    print("Sinyal döngüsü başlatıldı.")
-    
-    # Sonsuz döngüde sistemin açık kalması sağlanır
-    while True:
-        await asyncio.sleep(3600)
+    print("Bot başlatılıyor...")
+    await start_signal_loop()
 
 if __name__ == "__main__":
     asyncio.run(main())
