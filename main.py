c33db2f5d58866783b2a46503c6e6181f55a46c6
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-14 17:49:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-# main.py (geçici test tetiklemesi için)
-
 import asyncio
 from signal_loop import start_signal_loop
 
+async def main():
+    print("Sinyal kontrol sistemi başlatılıyor...")
+    while True:
+        await start_signal_loop()
+        await asyncio.sleep(180)
+
 if __name__ == "__main__":
-    print("Sistem başlatıldı. İlk sinyal testi için bekleniyor...")
-    asyncio.run(start_signal_loop())
+    asyncio.run(main())
