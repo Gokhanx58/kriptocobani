a23e7c91b3fb0d7ddf2df08ba5c40e2847c97158
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-14 16:36:14 +0300

    Update telegram_send.py

--- telegram_send.py
+++ telegram_send.py
@@ -1,13 +1,10 @@
-# telegram_send.py (renkli, sade, fiyatlı + ilk mesaj tetikleyici)
+# telegram_send.py (renkli, sade, fiyatlı)
 
 from telegram import Bot
 
 BOT_TOKEN = "8002562873:AAHoMdOpiZEi2XILMmrwAOjtyKEWNMVLKcs"
 CHANNEL_ID = "@GoKriptoLine"
 bot = Bot(token=BOT_TOKEN)
-
-# İlk gönderim tetikleyicisi
-first_sent = set()
 
 async def send_signal_to_channel(symbol, interval, signal, price):
     emoji = "✅" if "AL" in signal else "❌" if "SAT" in signal else "⏳"
@@ -27,10 +24,7 @@ async def send_signal_to_channel(symbol, interval, signal, price):
         f"💰 Fiyat: {price:.4f}"
     )
 
-    key = f"{symbol}_{interval}"
-    if key not in first_sent:
-        try:
-            await bot.send_message(chat_id=CHANNEL_ID, text=mesaj)
-            first_sent.add(key)
-        except Exception as e:
-            print(f"Telegram gönderim hatası: {e}")
+    try:
+        await bot.send_message(chat_id=CHANNEL_ID, text=mesaj)
+    except Exception as e:
+        print(f"Telegram gönderim hatası: {e}")
