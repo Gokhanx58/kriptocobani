2455006fbfaeb2ad297ddce0fc721be2b086ab53
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-14 13:32:02 +0300

    Update telegram_send.py

--- telegram_send.py
+++ telegram_send.py
@@ -1,18 +1,12 @@
-# telegram_send.py
-
-import httpx
+from telegram import Bot
 
 BOT_TOKEN = "8002562873:AAHoMdOpiZEi2XILMmrwAOjtyKEWNMVLKcs"
 CHANNEL_ID = "@GoKriptoLine"
+bot = Bot(token=BOT_TOKEN)
 
-async def send_telegram_message(message: str):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHANNEL_ID,
-        "text": message,
-        "parse_mode": "HTML"
-    }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url, data=payload)
-        print(f"[Telegram] Status Code: {response.status_code}, Response: {response.text}")
+async def send_signal_to_channel(symbol, interval, signal):
+    mesaj = f"{symbol} | {interval} dk → Sinyal: {signal}"
+    try:
+        await bot.send_message(chat_id=CHANNEL_ID, text=mesaj)
+    except Exception as e:
+        print(f"Telegram gönderim hatası: {e}")
