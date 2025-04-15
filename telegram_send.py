77dfb45a2101f9f1e74630f44c0b05363dde314e
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 09:54:50 +0300

    Update telegram_send.py

--- telegram_send.py
+++ telegram_send.py
@@ -1,12 +1,12 @@
 from telegram import Bot
 
 BOT_TOKEN = "8002562873:AAHoMdOpiZEi2XILMmrwAOjtyKEWNMVLKcs"
-CHANNEL_ID = "@GoKriptoLine"
+CHANNEL_ID = "-1002556449131"  # Dikkat: bu ID özel kanal içindir
+
 bot = Bot(token=BOT_TOKEN)
 
-async def send_signal_to_channel(symbol, interval, signal):
-    mesaj = f"{symbol} | {interval} dk → Sinyal: {signal}"
-    try:
-        await bot.send_message(chat_id=CHANNEL_ID, text=mesaj)
-    except Exception as e:
-        print(f"Telegram gönderim hatası: {e}")
+try:
+    bot.send_message(chat_id=CHANNEL_ID, text="✅ Test mesajı: Bu mesaj gelirse bağlantı tamam.")
+    print("✅ Telegram'a mesaj gönderildi.")
+except Exception as e:
+    print(f"❌ HATA: {e}")
