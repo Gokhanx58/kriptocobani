4a350a604a6f7f669f7062a8a4f63f3b9a9cfc6a
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 10:23:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import asyncio
 from signal_loop import start_signal_loop
 
 if __name__ == "__main__":
-    print("Sistem başlatılıyor...")
+    print("🚀 Sistem başlatıldı. İlk sinyaller kontrol ediliyor...")
     asyncio.run(start_signal_loop())
