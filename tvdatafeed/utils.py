7357c490845fbe79ec8488a7722326daf7c93c6a
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 12:22:18 +0300

    Update utils.py

--- tvdatafeed/utils.py
+++ tvdatafeed/utils.py
@@ -1,22 +1,64 @@
 # tvdatafeed/utils.py
 
 import json
-import random
-import string
+import re
 
-def get_random_string(length=12):
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
 
-def tv_json_loads(json_str):
-    try:
-        return json.loads(json_str)
-    except json.JSONDecodeError:
-        return {}
+class Filter:
+    def __init__(self, left, operation, right):
+        self.left = left
+        self.operation = operation
+        self.right = right
 
-def create_tv_request_payload(symbol, exchange, interval):
+    def to_dict(self):
+        return {
+            "left": self.left,
+            "operation": self.operation,
+            "right": self.right,
+        }
+
+
+class ScriptFunction:
+    def __init__(self, function: str, args: list = None):
+        self.function = function
+        self.args = args or []
+
+    def to_dict(self):
+        return {
+            "function": self.function,
+            "args": self.args,
+        }
+
+
+def get_symbol(symbol: str, exchange: str):
     return {
-        "symbol": f"{exchange}:{symbol}",
-        "resolution": interval,
-        "from": None,  # To be filled with a timestamp
-        "to": None,    # To be filled with a timestamp
+        "description": symbol,
+        "exchange": exchange,
+        "type": "",
     }
+
+
+def get_interval(interval):
+    return {
+        "1m": "1",
+        "5m": "5",
+        "15m": "15",
+        "30m": "30",
+        "1h": "60",
+        "4h": "240",
+        "1d": "D",
+        "1w": "W",
+        "1M": "M",
+    }.get(interval, interval)
+
+
+def convert_to_json(data):
+    return json.dumps(data, separators=(",", ":"))
+
+
+def extract_json(raw):
+    pattern = r"\{.*\}"
+    matches = re.findall(pattern, raw)
+    if matches:
+        return json.loads(matches[0])
+    raise ValueError("No JSON found")
