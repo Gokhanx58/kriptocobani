b58c2777973b2830c5017c10a638a35d0d5435da
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 11:18:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# main.py (asyncio ile sinyal döngüsünü başlatır)
+
 import asyncio
 from signal_loop import start_signal_loop
 
 if __name__ == "__main__":
-    print("🚀 Ana döngü çalışmaya başladı.")  # DEBUG log
+    print("🚀 Sinyal botu başlatıldı... İlk analizler gönderilecek.")
     asyncio.run(start_signal_loop())
