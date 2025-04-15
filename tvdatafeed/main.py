9db84aa03eb2a5353ae952adcc638d57d25e5a7e
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 15:43:02 +0300

    Update main.py

--- tvdatafeed/main.py
+++ tvdatafeed/main.py
@@ -1,7 +1,11 @@
-# main.py
-import asyncio
-from signal_loop import start_signal_loop
+from .tvsession import TVSession
+from .interval import Interval
 
-if __name__ == "__main__":
-    print("🚀 Bot başlatılıyor...")
-    asyncio.run(start_signal_loop())
+class TvDatafeed:
+    def __init__(self, username=None, password=None, auto_login=True):
+        self.session = TVSession()
+        if auto_login and username and password:
+            self.session.login(username, password)
+
+    def get_hist(self, symbol, exchange, interval=Interval.in_1_minute, n_bars=100):
+        return self.session.get_hist(symbol, exchange, interval, n_bars)
