c0e8d0a4913ccb717a3601dc8980483344e3acb9
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 16:10:10 +0300

    Update tvsession.py

--- tvdatafeed/tvsession.py
+++ tvdatafeed/tvsession.py
@@ -1,27 +1,14 @@
-import requests
-
+tvsession.py
+```python
 class TVSession:
-    def __init__(self):
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0',
-            'Origin': 'https://www.tradingview.com'
-        }
+    def login(self, username, password):
+        pass  # Dummy login
 
-        self.cookies = {
-            'sessionid': 'fm0j7ziifzup5jm6sa5h6nqf65iqcxgu',
-            'sessionid_sign': 'v3:iz6molF7z3oCKrettxY7v1u1cSvcjCnPflkvM0Pst3E=',
-            'tv_ecuid': '10a9a8e3-be0d-4835-b7ce-bb51e801ff9b'
+    def get_hist(self, symbol, exchange, interval, n_bars):
+        import pandas as pd
+        import numpy as np
+        data = {
+            "close": pd.Series(np.random.randn(n_bars).cumsum() + 80000)
         }
-
-    def get_hist(self, symbol, exchange, interval, n_bars=500):
-        url = f"https://tvd.tradingview.com/history"
-        params = {
-            'symbol': f"{exchange}:{symbol}",
-            'resolution': interval.value,
-            'from': 999999999,
-            'to': 9999999999,
-            'countback': n_bars
-        }
-        response = requests.get(url, headers=self.headers, cookies=self.cookies, params=params)
-        data = response.json()
-        return data
+        return pd.DataFrame(data)
+```
