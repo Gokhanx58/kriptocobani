0837820f7d678b9e928c43604a6ed1e88edbde3d
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 14:29:05 +0300

    Update interval.py

--- tvdatafeed/interval.py
+++ tvdatafeed/interval.py
@@ -1,14 +1,5 @@
-# tvdatafeed/interval.py
+from enum import Enum
 
-class Interval:
-    in_1_minute = "1"
-    in_3_minute = "3"
-    in_5_minute = "5"
-    in_15_minute = "15"
-    in_30_minute = "30"
-    in_1_hour = "60"
-    in_2_hour = "120"
-    in_4_hour = "240"
-    in_1_day = "1D"
-    in_1_week = "1W"
-    in_1_month = "1M"
+class Interval(Enum):
+    in_1_minute = '1'
+    in_5_minute = '5'
