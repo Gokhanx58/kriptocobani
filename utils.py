759c6de2184f2574bf118c4f6013f54bd4a3a3d5
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-15 15:31:07 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-def round_to_nearest(price: float, step: float) -> float:
-    """
-    Fiyatı en yakın adıma yuvarlar.
-    Örn: round_to_nearest(0.01983, 0.002) -> 0.02
-    """
-    return round(round(price / step) * step, 6)
+# utils.py
+
+def round_to_nearest(value, precision=0.0001):
+    if value is None:
+        return "-"
+    return round(value, 4)
