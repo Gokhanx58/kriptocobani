fa1bb841ba6fd309eb47d0a9e9bd419a743959ea
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 16:18:51 +0300

    Update fvg_detector.py

--- fvg_detector.py
+++ fvg_detector.py
@@ -1,24 +1,25 @@
 import pandas as pd
 
-def detect_fvg(df):
+def detect_fvg(df, min_gap_percent=0.1):
     fvg_zones = []
-
     for i in range(2, len(df)):
-        high_2 = df['high'].iloc[i - 2]
+        high_0 = df['high'].iloc[i - 2]
+        low_1 = df['low'].iloc[i - 1]
         low_2 = df['low'].iloc[i - 2]
         high_1 = df['high'].iloc[i - 1]
-        low_1 = df['low'].iloc[i - 1]
-        high_0 = df['high'].iloc[i]
-        low_0 = df['low'].iloc[i]
 
-        time = df.index[i]
+        # FVG Up (boşluk yukarı yönlü)
+        if low_1 > high_0:
+            gap_size = low_1 - high_0
+            gap_percent = (gap_size / high_0) * 100
+            if gap_percent >= min_gap_percent:
+                fvg_zones.append((df.index[i], 'FVG_UP', high_0, low_1))
 
-        # Bullish FVG (Up Gap)
-        if low_0 > high_2:
-            fvg_zones.append((time, 'FVG_UP', high_2, low_0))
-
-        # Bearish FVG (Down Gap)
-        elif high_0 < low_2:
-            fvg_zones.append((time, 'FVG_DOWN', high_0, low_2))
+        # FVG Down (boşluk aşağı yönlü)
+        if high_1 < low_2:
+            gap_size = low_2 - high_1
+            gap_percent = (gap_size / low_2) * 100
+            if gap_percent >= min_gap_percent:
+                fvg_zones.append((df.index[i], 'FVG_DOWN', high_1, low_2))
 
     return fvg_zones
