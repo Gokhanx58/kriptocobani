bdae44bf084e7510aeea8a75d0b67acb987a36f9
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 13:55:36 +0300

    Update utils.py

--- tvdatafeed/utils.py
+++ tvdatafeed/utils.py
@@ -1,13 +1,1 @@
-from .const import Interval
 
-def get_interval(interval_str: str) -> Interval:
-    mapping = {
-        "1m": Interval.MIN_1,
-        "5m": Interval.MIN_5,
-        "15m": Interval.MIN_15,
-        "30m": Interval.MIN_30,
-        "1h": Interval.HOUR_1,
-        "4h": Interval.HOUR_4,
-        "1d": Interval.DAY_1,
-    }
-    return mapping.get(interval_str, Interval.MIN_1)
