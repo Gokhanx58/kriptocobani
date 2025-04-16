c463b4179e0a99fcfc9d1363f23c714e3672b334
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 12:38:39 +0300

    Update tvsession.py

--- tvdatafeed/tvsession.py
+++ tvdatafeed/tvsession.py
@@ -1,34 +1,5 @@
-import requests
-
-
 class TvSession:
-    def __init__(self, username=None, password=None):
-        self.username = username
-        self.password = password
-        self.session = requests.Session()
-        self.headers = {
-            "User-Agent": "Mozilla/5.0",
-            "Referer": "https://www.tradingview.com"
-        }
-
-        if self.username and self.password:
-            self.login()
-
-    def login(self):
-        try:
-            url = "https://www.tradingview.com/accounts/signin/"
-            payload = {
-                "username": self.username,
-                "password": self.password,
-                "remember": "on"
-            }
-            response = self.session.post(url, json=payload, headers=self.headers)
-
-            if response.status_code == 200 and "sessionid" in response.cookies:
-                print("✅ TradingView oturumu başarıyla başlatıldı.")
-            else:
-                print("❌ Giriş başarısız. Yanıt kodu:", response.status_code)
-                print(response.text)
-
-        except Exception as e:
-            print(f"⚠️ Giriş sırasında hata oluştu: {e}")
+    def __init__(self):
+        self.sessionid = "fm0j7ziifzup5jm6sa5h6nqf65iqcxgu"
+        self.sessionid_sign = "v3:iz6molF7z3oCKrettxY7v1u1cSvcjCnPflkvM0Pst3E="
+        self.tv_ecuid = "10a9a8e3-be0d-4835-b7ce-bb51e801ff9b"
