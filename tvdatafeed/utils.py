df1449e4d84690a2f5b037e123a3f9d3b19692c4
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 12:38:58 +0300

    Update utils.py

--- tvdatafeed/utils.py
+++ tvdatafeed/utils.py
@@ -1,11 +1,1 @@
-from enum import Enum
-import pandas as pd
 
-class Interval(Enum):
-    in_1_minute = "1m"
-    in_5_minute = "5m"
-
-def convert_to_df(data):
-    df = pd.DataFrame(data)
-    df.set_index('datetime', inplace=True)
-    return df
