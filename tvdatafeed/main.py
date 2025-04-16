35b4767c7a935d1098efa1d1b4353e24004a3a6a
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 13:21:59 +0300

    Update main.py

--- tvdatafeed/main.py
+++ tvdatafeed/main.py
@@ -1,9 +1,17 @@
 from .session import TvSession
+from .utils import get_interval
 from .const import Interval
 
-class TvDatafeed:
-    def __init__(self):
-        self.session = TvSession()
 
-    def get_hist(self, symbol, interval, exchange="BINANCE", n_bars=200):
-        return []
+class TvDatafeed:
+    def __init__(self, username=None, password=None):
+        self.session = TvSession(username, password)
+
+    def get_hist(self, symbol, exchange, interval: Interval, n_bars=1000):
+        interval_str = interval.value
+        return self.session.get_hist(
+            symbol=symbol,
+            exchange=exchange,
+            interval=interval_str,
+            n_bars=n_bars
+        )
