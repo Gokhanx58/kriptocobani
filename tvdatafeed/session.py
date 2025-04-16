8abac4f4516c463e72602dc5f7c15c7e4de597dc
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 12:56:11 +0300

    Create session.py

---
+++ tvdatafeed/session.py
@@ -0,0 +1,33 @@
+import requests
+
+class TvSession:
+    def __init__(self, username=None, password=None, session=None):
+        self.username = username
+        self.password = password
+
+        if session:
+            self.session = session
+        elif username and password:
+            self.session = self.login(username, password)
+        else:
+            raise ValueError("Username/password veya session bilgisi gerekli!")
+
+    def login(self, username, password):
+        login_url = "https://www.tradingview.com/accounts/signin/"
+        headers = {
+            "Referer": "https://www.tradingview.com",
+            "User-Agent": "Mozilla/5.0"
+        }
+        data = {
+            "username": username,
+            "password": password
+        }
+
+        response = requests.post(login_url, json=data, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"Giriş başarısız. Kod: {response.status_code}, Detay: {response.text}")
+
+        session = requests.Session()
+        session.headers.update(headers)
+        session.cookies.update(response.cookies)
+        return session
