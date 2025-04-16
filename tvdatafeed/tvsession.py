d663ee6121c7046876faca5050f4c065141c15df
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 12:51:58 +0300

    Update tvsession.py

--- tvdatafeed/tvsession.py
+++ tvdatafeed/tvsession.py
@@ -1,5 +1,34 @@
+import requests
+
+
 class TvSession:
-    def __init__(self):
-        self.sessionid = "fm0j7ziifzup5jm6sa5h6nqf65iqcxgu"
-        self.sessionid_sign = "v3:iz6molF7z3oCKrettxY7v1u1cSvcjCnPflkvM0Pst3E="
-        self.tv_ecuid = "10a9a8e3-be0d-4835-b7ce-bb51e801ff9b"
+    def __init__(self, username=None, password=None, session=None):
+        self.username = username
+        self.password = password
+
+        if session:
+            self.session = session
+        elif username and password:
+            self.session = self.login(username, password)
+        else:
+            raise ValueError("Username/password veya session bilgisi gerekli!")
+
+    def login(self, username, password):
+        login_url = "https://www.tradingview.com/accounts/signin/"
+        headers = {
+            "Referer": "https://www.tradingview.com",
+            "User-Agent": "Mozilla/5.0"
+        }
+        data = {
+            "username": username,
+            "password": password
+        }
+
+        response = requests.post(login_url, json=data, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"Giriş başarısız. Kod: {response.status_code}, Detay: {response.text}")
+
+        session = requests.Session()
+        session.headers.update(headers)
+        session.cookies.update(response.cookies)
+        return session
