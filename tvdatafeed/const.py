dfd34155c2c57877cc9f358aec05ddc824d24477
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 10:59:44 +0300

    Update const.py

--- tvdatafeed/const.py
+++ tvdatafeed/const.py
@@ -1,4 +1,30 @@
 INTERVALS = {
     "1m": "1",
-    "5m": "5"
+    "5m": "5",
+    "15m": "15",
+    "30m": "30",
+    "1h": "60",
+    "4h": "240",
+    "1d": "D",
+    "1w": "W",
+    "1M": "M"
+}
+
+SCREENER = "crypto"
+
+GET_HISTORY_PAYLOAD = {
+    "symbol": "",
+    "resolution": "",
+    "from": 0,
+    "to": 0,
+    "countback": 300,
+    "currencyCode": "USD"
+}
+
+WEBSOCKET_HEADERS = {
+    "Connection": "Upgrade",
+    "Upgrade": "websocket",
+    "Host": "prodata.tradingview.com",
+    "Origin": "https://www.tradingview.com",
+    "User-Agent": "Mozilla/5.0"
 }
