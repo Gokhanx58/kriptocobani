0506bc8c378a26e194d41219616a420805a1d4b4
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 10:36:27 +0300

    Update utils.py

--- tvdatafeed/utils.py
+++ tvdatafeed/utils.py
@@ -1,2 +1,11 @@
-def round_to_nearest(x, step):
-    return round(x / step) * step
+from enum import Enum
+import pandas as pd
+
+class Interval(Enum):
+    in_1_minute = "1m"
+    in_5_minute = "5m"
+
+def convert_to_df(data):
+    df = pd.DataFrame(data)
+    df.set_index('datetime', inplace=True)
+    return df
