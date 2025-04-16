b8d2bbfa56698511c5622a1a3cefc393e16ca956
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 14:09:33 +0300

    Update signal_loop.py

--- signal_loop.py
+++ signal_loop.py
@@ -2,6 +2,10 @@ import asyncio
 from analyzer import analyze_signals
 
 async def start_signal_loop():
+    # İlk sinyal analizini hemen yap
+    await analyze_signals()
+
+    # Sonsuz döngüde sadece sinyal değişimi oldukça tekrar çağrılır
     while True:
+        await asyncio.sleep(15)  # Çok düşük gecikme, spam engelleme amaçlı küçük bekleme
         await analyze_signals()
-        await asyncio.sleep(180)
