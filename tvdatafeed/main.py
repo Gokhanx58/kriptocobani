3fe79f0933b739392b16b32f96d98e5dfbbe9ac3
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 09:48:30 +0300

    Update main.py

--- tvdatafeed/main.py
+++ tvdatafeed/main.py
@@ -1,11 +1,28 @@
-from .tvsession import TVSession
-from .interval import Interval
+from tvdatafeed.interval import Interval
+from tvdatafeed.tvsession import TvSession
+import pandas as pd
 
 class TvDatafeed:
-    def __init__(self, username=None, password=None, auto_login=True):
-        self.session = TVSession()
-        if auto_login and username and password:
-            self.session.login(username, password)
+    def __init__(self):
+        self.session = TvSession()
 
-    def get_hist(self, symbol, exchange, interval=Interval.in_1_minute, n_bars=100):
-        return self.session.get_hist(symbol, exchange, interval, n_bars)
+    def get_hist(self, symbol: str, exchange: str, interval: Interval, n_bars: int = 100):
+        url = f"https://api.binance.com/api/v3/klines"
+        params = {
+            "symbol": symbol.replace("/", "").upper(),
+            "interval": interval.value,
+            "limit": n_bars
+        }
+        data = self.session.get_data(url, params)
+        if not data:
+            return None
+
+        df = pd.DataFrame(data, columns=[
+            'datetime', 'open', 'high', 'low', 'close', 'volume',
+            'close_time', 'quote_asset_volume', 'number_of_trades',
+            'taker_buy_base_volume', 'taker_buy_quote_volume', 'ignore'
+        ])
+        df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+        df.set_index('datetime', inplace=True)
+        df = df.astype(float)
+        return df
