0c57200eafaade66917f1ebfa45fef7e46f1194f
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 09:14:01 +0300

    Update signal_loop.py

--- signal_loop.py
+++ signal_loop.py
@@ -2,12 +2,6 @@ import asyncio
 from analyzer import analyze_signals
 
 async def start_signal_loop():
-    print("✅ Sinyal döngüsü başladı.")
-    await analyze_signals(initial=True)
-
     while True:
-        try:
-            await analyze_signals()
-        except Exception as e:
-            print(f"⚠️ Döngü hatası: {e}")
+        analyze_signals()
         await asyncio.sleep(180)
