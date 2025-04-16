2c7f8d421ec04b6eee9a03079abb5e6a30580866
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 15:49:06 +0300

    Update fvg_detector.py

--- fvg_detector.py
+++ fvg_detector.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
 def detect_fvg(df: pd.DataFrame):
-    df = df.copy()
     fvg_zones = []
 
     for i in range(2, len(df)):
-        prev = df.iloc[i - 2]
-        mid = df.iloc[i - 1]
-        curr = df.iloc[i]
-        if mid['low'] > prev['high'] and mid['low'] > curr['high']:
-            fvg_zones.append((df.index[i - 1], 'FVG_DOWN', mid['low'], prev['high']))
-        elif mid['high'] < prev['low'] and mid['high'] < curr['low']:
-            fvg_zones.append((df.index[i - 1], 'FVG_UP', mid['high'], prev['low']))
+        # FVG_DOWN: önceki iki mumun low'u > son mumun high'ı
+        if df['low'].iloc[i - 2] > df['high'].iloc[i - 1] and df['low'].iloc[i - 1] > df['high'].iloc[i]:
+            ts = df.index[i]
+            fvg_zones.append((ts, 'FVG_DOWN'))
+
+        # FVG_UP: önceki iki mumun high'ı < son mumun low'u
+        elif df['high'].iloc[i - 2] < df['low'].iloc[i - 1] and df['high'].iloc[i - 1] < df['low'].iloc[i]:
+            ts = df.index[i]
+            fvg_zones.append((ts, 'FVG_UP'))
+
+    print("Tespit edilen FVG'ler:", fvg_zones)
     return fvg_zones
