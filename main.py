ac9cb56c5c76d5b7447326bc19a4df0d5a6ebe93
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 10:55:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from signal_loop import start_signal_loop
 
+print("🚀 Bot başlatılıyor...")
+
 if __name__ == "__main__":
-    print("\U0001F680 Bot başlatılıyor...")
     asyncio.run(start_signal_loop())
