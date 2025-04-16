2a51501c7153723300205b7c2e8c7e9a673af606
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-16 11:56:49 +0300

    Update main.py

--- tvdatafeed/main.py
+++ tvdatafeed/main.py
@@ -1,7 +1,24 @@
-from signal_loop import start_signal_loop
-import asyncio
+from .tvsession import TvSession
+from .utils import get_hist, search_symbol
+from .const import Interval
 
-print("\n🚀 Sistem başlatılıyor...")
 
-if __name__ == "__main__":
-    asyncio.run(start_signal_loop())
+class TvDatafeed:
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
+        self.session = TvSession()
+        if username and password:
+            self.session.login(username, password)
+
+    def get_hist(self, symbol: str, exchange: str, interval: Interval = Interval.MIN_1, n_bars: int = 500):
+        return get_hist(
+            symbol=symbol,
+            exchange=exchange,
+            interval=interval,
+            n_bars=n_bars,
+            session=self.session
+        )
+
+    def search_symbol(self, text: str):
+        return search_symbol(text, self.session)
