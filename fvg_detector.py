2486361fd11b3b27bdf92a8d42477fb0339c76c0
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-17 10:55:03 +0300

    Update fvg_detector.py

--- fvg_detector.py
+++ fvg_detector.py
@@ -2,26 +2,14 @@ import pandas as pd
 import logging
 
 def detect_fvg_zones(df: pd.DataFrame):
-    """
-    Fair Value Gap tespiti yapar ve (timestamp, type, gap_high, gap_low) listesi döner.
-    """
-    fvg = []
+    fvgs = []
     for i in range(2, len(df)):
-        h2 = df['high'].iat[i-2]
-        l1 = df['low'].iat[i-1]
-        l0 = df['low'].iat[i]
-        h1 = df['high'].iat[i-1]
-
-        ts = df.index[i]
-        # Boşluk (down gap): önceki low > two bars ago high
-        if l1 > h2:
-            fvg.append((ts, "FVG_DOWN", l1, h2))
-            logging.info(f"FVG_DOWN @ {ts}")
-
-        # Boşluk (up gap): önceki high < current low
-        elif h1 < l0:
-            fvg.append((ts, "FVG_UP", l0, h1))
-            logging.info(f"FVG_UP @ {ts}")
-
-    logging.debug(f"FVG_ZONES: {fvg}")
-    return fvg
+        high_2, mid_low, low_0 = df.high.iat[i-2], df.low.iat[i-1], df.low.iat[i]
+        if mid_low > high_2:
+            fvgs.append((df.index[i], "FVG_DOWN", mid_low, high_2))
+        else:
+            high_1 = df.high.iat[i-1]
+            if high_1 < low_0:
+                fvgs.append((df.index[i], "FVG_UP", low_0, high_1))
+    logging.debug(f"FVG ZONES: {fvgs}")
+    return fvgs
