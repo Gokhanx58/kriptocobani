165bddc68eb368fdb2f99ae5d0654d82e6e8c707
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-17 10:42:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 from tvdatafeed import Interval
 
+# Telegram
 TELEGRAM_TOKEN = "7677308602:AAHH7vloPaQ7PqgFdBnJ2DKYy6sjJ5iqaYE"
 TELEGRAM_CHANNEL = "@GokriptoHan"
 
-SYMBOLS = ["BTCUSDT", "ETHUSDT", "SUIUSDT", "SOLUSDT", "AVAXUSDT"]
+# Semboller / Zaman dilimleri / Bar sayısı
+SYMBOLS = ["BTCUSDT", "ETHUSDT", "SOLUSDT", "AVAXUSDT", "SUIUSDT"]
 INTERVALS = [Interval.MIN_1, Interval.MIN_5]
-
 BARS = 200
+
+# (İleride kullanmak için placeholder)
+TOLERANCE = 0.0005
