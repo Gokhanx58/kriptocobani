479fead520561ad43d185ec0fbb7dd560d08e3f0
Gokhanx58 <gokhanzkocak@gmail.com>
2025-04-17 11:05:49 +0300

    Update __init__.py

--- tvdatafeed/__init__.py
+++ tvdatafeed/__init__.py
@@ -1,2 +1,14 @@
 from .main import TvDatafeed
 from .interval import Interval
+
+# tvdatafeed/interval.py
+from enum import Enum
+
+class Interval(Enum):
+    MIN_1 = '1m'
+    MIN_5 = '5m'
+    MIN_15 = '15m'
+    MIN_30 = '30m'
+    HOUR_1 = '1h'
+    HOUR_4 = '4h'
+    DAY_1 = '1d'
